998f6e7850e68a9a06dcf5b497708131ff955512
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2018-01-19 21:02:16 -0500

    started building new test site, added navbar

---
+++ index.php
@@ -0,0 +1,147 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <title>Environmental Dashboard</title>
+  </head>
+  <body>
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <img src="images/banner.jpg" alt="" class="img-fluid">
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+              <ul class="navbar-nav">
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Citywide Dashboard
+                  </a>
+                  <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink1">
+                    <a class="dropdown-item" href="#">Citywide View</a>
+                    <a class="dropdown-item" href="#">Electricity</a>
+                    <a class="dropdown-item" href="#">Water Flow</a>
+                    <a class="dropdown-item" href="#">Water Quality</a>
+                    <a class="dropdown-item" href="#">10 Acre Solar Array</a>
+                    <a class="dropdown-item" href="#">Gauges explained</a>
+                  </div>
+                </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Building Dashboard
+                  </a>
+                  <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink2">
+                    <a class="dropdown-item" href="#">Building Dashboard Explained</a>
+
+                    <a class="dropdown-item dropdown-toggle" href="#" id="navbarDropdownMenuLink3" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Oberlin City</a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink3">
+                        <li><a class="dropdown-item" href="#">Eastwood Elementary</a></li>
+                        <li><a class="dropdown-item" href="#">Prospect Elementary</a></li>
+                        <li><a class="dropdown-item" href="#">Langston Middle School</a></li>
+                        <li><a class="dropdown-item" href="#">Oberlin High School</a></li>
+                        <li><a class="dropdown-item" href="#">Langston Board Office</a></li>
+                        <li><a class="dropdown-item" href="#">Oberlin Public Library</a></li>
+                    </ul>
+
+                    <a class="dropdown-item dropdown-toggle" href="#" id="navbarDropdownMenuLink4" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Oberlin College</a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink4">
+                        <li><a class="dropdown-item" href="#">Eastwood Elementary</a></li>
+                        <li><a class="dropdown-item" href="#">Prospect Elementary</a></li>
+                        <li><a class="dropdown-item" href="#">Langston Middle School</a></li>
+                        <li><a class="dropdown-item" href="#">Oberlin High School</a></li>
+                        <li><a class="dropdown-item" href="#">Langston Board Office</a></li>
+                        <li><a class="dropdown-item" href="#">Oberlin Public Library</a></li>
+                    </ul>
+                  </div>
+                </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink5" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Community Voices
+                  </a>
+                  <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink5">
+                    <a class="dropdown-item" href="#">View Slides</a>
+                    <a class="dropdown-item" href="#">View Articles</a>
+                    <a class="dropdown-item" href="#">Advanced Image Search</a>
+                  </div>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Events Calendar</a>
+                </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink6" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    More
+                  </a>
+                  <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink6">
+                    <a class="dropdown-item" href="#">Resources Explained</a>
+                    <a class="dropdown-item" href="#">Story of Dashboard</a>
+                    <a class="dropdown-item" href="#">Bring Dashboard to Your Community</a>
+
+                    <a class="dropdown-item dropdown-toggle" href="#" id="navbarDropdownMenuLink7" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Instructor Toolkit</a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink7">
+                        <li><a class="dropdown-item" href="#">About</a></li>
+                        <li><a class="dropdown-item" href="#">Search K12</a></li>
+                        <li><a class="dropdown-item" href="#">Teacher Workshop</a></li>
+                    </ul>
+
+                    <a class="dropdown-item dropdown-toggle" href="#" id="navbarDropdownMenuLink8" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">About Us</a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink8">
+                        <li><a class="dropdown-item" href="#">Mission</a></li>
+                        <li><a class="dropdown-item" href="#">Meet the Team</a></li>
+                        <li><a class="dropdown-item" href="#">In the News</a></li>
+                    </ul>
+
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </div>
+      </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script>
+/*!
+ * Bootstrap 4 multi dropdown navbar ( https://bootstrapthemes.co/demo/resource/bootstrap-4-multi-dropdown-navbar/ )
+ * Copyright 2017.
+ * Licensed under the GPL license
+ */
+$(document).ready(function() {
+  $('.dropdown-menu a.dropdown-toggle').on('click', function(e) {
+    var $el = $(this);
+    var $parent = $(this).offsetParent(".dropdown-menu");
+    if (!$(this).next().hasClass('show')) {
+      $(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
+    }
+    var $subMenu = $(this).next(".dropdown-menu");
+    $subMenu.toggleClass('show');
+
+    $(this).parent("li").toggleClass('show');
+
+    $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function(e) {
+      $('.dropdown-menu .show').removeClass("show");
+    });
+
+    if (!$parent.parent().hasClass('navbar-nav')) {
+      $el.next().css({
+        "top": $el[0].offsetTop,
+        "left": $parent.outerWidth() - 4
+      });
+    }
+
+    return false;
+  });
+});
+
+    </script>
+  </body>
+</html>
