3b15b532a6846f2cc193b818ade94cca6cdf2176
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2018-01-23 10:49:30 -0500

    fixed link

--- cwd.php
+++ cwd.php
@@ -9,7 +9,7 @@
   <body>
     <div class="container">
       <?php include 'includes/header.php'; ?>
-      <object type="image/svg+xml" data="//environmentaldashboard.org/cwd/dashboard.php?<?php echo http_build_query($_GET); ?>" class='img-fluid'></object>
+      <object type="image/svg+xml" data="//environmentaldashboard.org/cwd-files/dashboard.php?<?php echo http_build_query($_GET); ?>" class='img-fluid'></object>
       <?php include 'includes/footer.php'; ?>
     </div>
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
