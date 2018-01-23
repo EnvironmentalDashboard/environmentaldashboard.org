b8922db3758e5cdab25e5251084085cf0e6f2a93
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2018-01-23 10:35:43 -0500

    about to upload

---
+++ story-of-dashboard.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="css/bootstrap.css?v=<?php echo time(); ?>">
+    <title>Environmental Dashboard</title>
+  </head>
+  <body>
+    <div class="container">
+      <?php include 'includes/header.php'; ?>
+      <div class='embed-responsive embed-responsive-16by9'><iframe class="embed-responsive-item" src="https://docs.google.com/presentation/d/1AosesvyjnPjp732lvuqF6qgeB8uGh0diTAEyVQuEZBc/embed?start=false&loop=false&delayms=30000" frameborder="0"></iframe></div>
+      <h3 class="text-center">CampusEnergy2017 Student Video Contest: Oberlin College (Like our video!)</h3>
+      <div class='embed-responsive embed-responsive-16by9'><iframe class="embed-responsive-item" src="https://www.youtube-nocookie.com/embed/1H49cDTKTX0?rel=0" frameborder="0"></iframe></div>
+      <h3 class="text-center">Learn more about the Environmental Dashboard from members of the team!</h3>
+      <div class='embed-responsive embed-responsive-16by9'><iframe class="embed-responsive-item" src="https://www.youtube-nocookie.com/embed/eNTC7dDEAsU?rel=0&showinfo=0" frameborder="0"></iframe></div>
+      <div style="clear: both;height: 30px"></div>
+      <?php include 'includes/footer.php'; ?>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
