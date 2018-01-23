b9e09bf8935febe3a06c81e96ed326c941421f7e
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2018-01-23 11:02:06 -0500

    fixed link

--- includes/footer.php
+++ includes/footer.php
@@ -1,27 +1,27 @@
 <div class="row text-center justify-content-md-center">
   <div class="col-3 col-md-1">
-    <img src="http://environmentaldashboard.org/wp-content/uploads/2015/08/sf1-300x227.jpg" alt="" class="img-fluid">
+    <img src="http://environmentaldashboard.org/images/uploads/2015/08/sf1-300x227.jpg" alt="" class="img-fluid">
   </div>
   <div class="col-3 col-md-1">
-    <img src="http://environmentaldashboard.org/wp-content/uploads/2015/08/op1-300x162.jpg" alt="" class="img-fluid">
+    <img src="http://environmentaldashboard.org/images/uploads/2015/08/op1-300x162.jpg" alt="" class="img-fluid">
   </div>
   <div class="col-3 col-md-1">
-    <img src="http://environmentaldashboard.org/wp-content/uploads/2015/08/glpf1-297x300.jpg" alt="" class="img-fluid">
+    <img src="http://environmentaldashboard.org/images/uploads/2015/08/glpf1-297x300.jpg" alt="" class="img-fluid">
   </div>
   <div class="col-3 col-md-1">
-    <img src="http://environmentaldashboard.org/wp-content/uploads/2015/07/ob-300x300.jpg" alt="" class="img-fluid">
+    <img src="http://environmentaldashboard.org/images/uploads/2015/07/ob-300x300.jpg" alt="" class="img-fluid">
   </div>
   <div class="col-3 col-md-1">
-    <img src="http://environmentaldashboard.org/wp-content/uploads/2015/07/epa-300x300.jpg" alt="" class="img-fluid">
+    <img src="http://environmentaldashboard.org/images/uploads/2015/07/epa-300x300.jpg" alt="" class="img-fluid">
   </div>
   <div class="col-3 col-md-1">
-    <img src="http://environmentaldashboard.org/wp-content/uploads/2015/07/glca-300x300.jpg" alt="" class="img-fluid">
+    <img src="http://environmentaldashboard.org/images/uploads/2015/07/glca-300x300.jpg" alt="" class="img-fluid">
   </div>
   <div class="col-3 col-md-1">
-    <img src="http://environmentaldashboard.org/wp-content/uploads/2015/08/lucid-300x252.jpg" alt="" class="img-fluid">
+    <img src="http://environmentaldashboard.org/images/uploads/2015/08/lucid-300x252.jpg" alt="" class="img-fluid">
   </div>
   <div class="col-3 col-md-1">
-    <img src="http://environmentaldashboard.org/wp-content/uploads/2015/07/oc-300x300.jpg" alt="" class="img-fluid">
+    <img src="http://environmentaldashboard.org/images/uploads/2015/07/oc-300x300.jpg" alt="" class="img-fluid">
   </div>
 </div>
 <div class="row">
@@ -29,7 +29,7 @@
 </div>
 <div class="row">
   <div class="col text-center" style="padding-bottom: 50px">
-    <a href="#" class="btn btn-primary" style="height: 35px;width: 35px;padding: 5px"><img src="http://67.205.179.187/test/environmentaldashboard.org/images/facebook-f.svg" alt="" style="height: 100%"></a>
-    <a href="#" class="btn btn-primary" style="height: 35px;width: 35px;padding: 5px"><img src="http://67.205.179.187/test/environmentaldashboard.org/images/twitter.svg" style="height: 100%" alt=""></a>
+    <a href="#" class="btn btn-primary" style="height: 35px;width: 35px;padding: 5px"><img src="http://environmentaldashboard.org/images/facebook-f.svg" alt="" style="height: 100%"></a>
+    <a href="#" class="btn btn-primary" style="height: 35px;width: 35px;padding: 5px"><img src="http://environmentaldashboard.org/images/twitter.svg" style="height: 100%" alt=""></a>
   </div>
 </div>
