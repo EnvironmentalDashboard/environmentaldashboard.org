c04540a80fb6fd065db0a1ff05ecc1599a212415
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2018-01-24 00:18:13 -0500

    working on teacher resources

---
+++ edresources/searchedresources.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="/css/bootstrap.css?v=<?php echo time(); ?>">
+    <title>Environmental Dashboard</title>
+  </head>
+  <body>
+    <div class="container">
+      <?php include 'includes/header.php'; ?>
+      <div class="row" style="padding: 30px">
+        <div class="col-12">
+          <h1 class="text-center">Search K-12 Instructor Toolkit</h1>
+          <p>The Search table below allows you to search through our entire library of lessons easily and efficiently. Use the search box to search lesson titles or one of our logical operators to refine your search. On this site, you will be able to search for Environmental Dashboard specific lessons as well as external teaching resources that utilize Environmental Dashboard as a tool.</p>
+          <p>For example, you could search for a lesson with the keyword "electricity" to see all lessons tagged or titled "electricity" with their respective sub data for quick assessment. You could also add use any of the other search parameters such as "grade" and "author(s)". To reset your search, simply click "reset" icon. The number in the brackets indicate the total quantity of lessons for each specific field you include in a search.</p>
+          <div class="card border-primary mb-3" style="max-width: 18rem;">
+            <div class="card-header bg-transparent border-primary">Header</div>
+            <div class="card-body text-primary">
+              <h5 class="card-title">primary card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            <div class="card-footer bg-transparent border-primary">Footer</div>
+          </div>
+        </div>
+      </div>
+      <?php include 'includes/footer.php'; ?>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ edresources/workshops.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="/css/bootstrap.css?v=<?php echo time(); ?>">
+    <title>Environmental Dashboard</title>
+  </head>
+  <body>
+    <div class="container">
+      <?php include 'includes/header.php'; ?>
+      <div class="row" style="padding: 30px">
+        <div class="col-12">
+          <h1 class="text-center">Curriculum Development Workshops</h1>
+          <p><i>Environmental Dashboard</i> (ED)  technology is being installed in school districts in Ohio and nationally.  ED is designed to make flows of water and energy through buildings and whole communities and environmental conditions visible, engaging and teachable while also linking these to smart environmental decision-making.  A grant from the Ohio Environmental Education Fund (Ohio EPA) is supporting a series of workshops designed to better integrate this technology into the K-12 curriculum.  Workshop goals are to enhance teaching of content associated with the core curriculum and state standards, and to develop students’ “systems thinking” skills within the context of existing lessons.</p>
+          <p>These workshops are a collaboration between four participating school districts and Oberlin College with support from from the Ohio Environmental Education Fund (Ohio EPA). Questions answered at <a href="mailto:dashboard@oberlin.edu">dashboard@oberlin.edu</a>.</p>
+          <p>The model for these workshops assumes that teachers and curricular managers in each district are in the best position to determine precisely how ED can best be integrated into existing curricula and to develop and help assess these exercises in the context of their own districts, schools and classrooms. This  “train the trainer” model is intended to develop a cadre of experienced and enthusiastic teachers with in-depth training in the use of ED.  These workshops includes teachers from Oberlin who already have experience developing and using ED curricular materials who can serve as mentors and experts.</p>
+          <h3>Curricular Development Workshops</h3>
+          <p>Teachers who complete workshop #1 are eligible to apply to participate in curriculum development.  Accepted applicants will participate in a sequence of four workshops and will develop, teach and assess at least one teaching unit that uses Environmental Dashboard (ED).</p>
+          <p>The overall goals for participants are to:</p>
+          <ul>
+            <li>Actively participate and support the learning community of participants from the four districts and Oberlin College</li>
+            <li>Propose and develop an ED related lesson and an assessment plan that aligns with existing curricula and learning goals in the districts and in Ohio</li>
+            <li>Develop additional knowledge and expertise related to areas of environment, decision-making and ED most relevant to the lesson proposed</li>
+            <li>Teach lesson(s) developed, assess learning</li>
+            <li>Present and receive feedback on lessons, revise accordingly</li>
+            <li>Work with curricular specialists to modify for posting in on-line ED-related lesson repository</li>
+          </ul>
+          <p>The professional development sequence:</p>
+          <ul>
+            <li>Summer 2017: Workshops</li>
+            <li>
+              <ul>
+                <li>Date: June 13, 2017 &amp; August 1, 2017</li>
+                <li>Location: Oberlin College</li>
+                <li>Outcome: Goals 1-3, Draft lesson plan(s), curriculum alignment</li>
+              </ul>
+            </li>
+            <li>Fall 2017: 2 webinars</li>
+            <li>
+              <ul>
+                <li>Dates: TBD</li>
+                <li>Location: Webinar</li>
+              </ul>
+            </li>
+            <li>Spring 2018: Workshop</li>
+            <li>
+              <ul>
+                <li>Date: TBD</li>
+                <li>Location: Oberlin College</li>
+                <li>Outcome: Share and Reflect on implementation</li>
+              </ul>
+            </li>
+          </ul>
+          <p>Educators will receive a total stipend of $500 after participating in the full sequence of workshops and producing lessons and will receive reimbursement for mileage. CEU credits will provided based on completing district requirements.  Pending the success of the program and grant funding, opportunities for further curriculum development may be possible.</p>
+          <p>Space in these workshops is limited to 20 educators.  Review of applications will begin on April 1 and will end when available space is filled</p>
+        </div>
+      </div>
+      <?php include 'includes/footer.php'; ?>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
