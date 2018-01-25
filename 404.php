696e803617553ce8d91cb7b115856e1e3c6dc4db
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2018-01-25 10:13:08 -0500

    added 404 page

---
+++ 404.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://environmentaldashboard.org/css/bootstrap.css?v=<?php echo time(); ?>">
+    <title>Environmental Dashboard</title>
+  </head>
+  <body>
+    <div class="container">
+      <?php include 'includes/header.php'; ?>
+      <div class="row" style="margin-top: 50px;margin-bottom: 400px">
+        <div class="col-12">
+          <h1 class="text-center">404 Error</h1>
+          <p class="text-center">The page you are looking for could not be found.</p>
+        </div>
+      </div>
+      <?php include 'includes/footer.php'; ?>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
