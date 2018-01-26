06077b97a32b1aed6ff176b31593409b51bbdfb1
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2018-01-26 13:39:28 -0500

    favicon

--- community-voices/image-search.php
+++ community-voices/image-search.php
@@ -105,6 +105,19 @@ parse_str($_SERVER['QUERY_STRING'], $qs);
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://environmentaldashboard.org/css/bootstrap.css?v=<?php echo time(); ?>">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=9ByOqqx0o3">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=9ByOqqx0o3">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=9ByOqqx0o3">
+    <link rel="manifest" href="/manifest.json?v=9ByOqqx0o3">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg?v=9ByOqqx0o3" color="#00a300">
+    <link rel="shortcut icon" href="/favicon.ico?v=9ByOqqx0o3">
+    <meta name="theme-color" content="#000000"><link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=9ByOqqx0o3">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=9ByOqqx0o3">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=9ByOqqx0o3">
+<link rel="manifest" href="/manifest.json?v=9ByOqqx0o3">
+<link rel="mask-icon" href="/safari-pinned-tab.svg?v=9ByOqqx0o3" color="#00a300">
+<link rel="shortcut icon" href="/favicon.ico?v=9ByOqqx0o3">
+<meta name="theme-color" content="#000000">
     <title>Environmental Dashboard</title>
   </head>
   <body>
