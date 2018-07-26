a51a36c55ecb4bc31d455ae8c3341081ac56fa71
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2018-07-26 19:47:33 -0400

    changes requested in "Cleaning up lessons on Teacher Resources" email

--- edresources/searchedresources.php
+++ edresources/searchedresources.php
@@ -18,15 +18,16 @@ if (isset($_GET['submit'])) {
       continue;
     }
     $sql .= "(`key` = ? AND value = ?) OR ";
-    $params[] = str_replace('$WS$', ' ', $key);
-    $params[] = str_replace('$WS$', ' ', $value);
+    $params[] = $key;//str_replace('$WS$', ' ', $key);
+    $params[] = $value;//str_replace('$WS$', ' ', $value);
   }
   if ($sql !== '') {
     $sql = 'WHERE ' . substr($sql, 0, -4); // remove the final ' OR '
   }
   $sql2 = '';
   if (strlen($search_query) > 0) {
-    $sql2 = ' AND title LIKE ?';
+    $sql2 = ' AND (title LIKE ? OR id IN (SELECT lesson_id FROM cv_lesson_meta WHERE value LIKE ?))';
+    $params[] = "%{$search_query}%";
     $params[] = "%{$search_query}%";
   }
   $stmt = $db->prepare("SELECT SQL_CALC_FOUND_ROWS id, title, pdf, published FROM cv_lessons WHERE id IN (SELECT lesson_id FROM cv_lesson_meta {$sql}){$sql2} ORDER BY published DESC LIMIT {$offset}, {$limit}");
@@ -38,6 +39,19 @@ if (isset($_GET['submit'])) {
   $query = false;
 }
 parse_str($_SERVER['QUERY_STRING'], $qs);
+function cmp($a, $b) {
+  $a = $a['value'];
+  $b = $b['value'];
+  if ($a == $b) {
+    return 0;
+  }
+  $a_ = explode(' ', $a);
+  $b_ = explode(' ', $b);
+  if ($a_[0] === 'Grade' && $b_[0] === 'Grade') {
+    return (intval($a_[1]) < intval($b_[1])) ? -1 : 1;
+  }
+  return ($a < $b) ? -1 : 1;
+}
 ?>
 <!doctype html>
 <html lang="en">
@@ -61,13 +75,12 @@ parse_str($_SERVER['QUERY_STRING'], $qs);
       <div class="row" style="padding: 30px">
         <div class="<?php echo($query) ? 'col-12 col-sm-3' : 'col-12' ?>">
           <?php if (!$query) { ?>
-          <h1 class="text-center">Search K-12 Instructor Toolkit</h1>
-          <p>On this site, you will be able to search for Environmental Dashboard specific lessons that utilize Dashboard as a tool.  The Search table below allows you to comb through our entire library of lessons easily and efficiently. Use the search box to find lessons by title or one of our search fields, such as subjects or student levels, to refine your search.</p>
-          <p>For example, you could search for a lesson with the keyword "electricity" to see all lessons tagged or titled "electricity" with their respective sub data for quick assessment. You could also add any of the other search parameters such as "grade" and "author(s)". To reset your search, simply click "reset" icon.</p>
+          <h1 class="text-center">Search Lessons and Units</h1>
+          <p>Educators from a variety of schools have creatively employed Environmental Dashboard to teach a variety of concepts, subjects and levels.  Use the selectors below to search our repository for Dashboard-related lessons and units by title, topic, grade level or any of the other search fields alone or in combination.</p>
+          <p>For example, a search for the term "electricity" will take you to titles and summary descriptions of all lessons tagged as being related.  From  this list you can click and download PDFs of complete lessons.  This <a href="https://docs.google.com/document/d/1uJLghYhpDRiZadqeeacH2aUyiedKHI0DhQs_QWsQjLM/edit">Navigation Guide to BuildingOS</a> may be a useful resource as a general introduction to the tools and apps in the BuildingOS component of Dashboard.</p>
           <?php } ?>
           <form action="" method="GET">
             <div class="card bg-light mb-3">
-              <div class="card-header bg-transparent">Search Lessons</div>
               <div class="card-body">
                 <div class="row">
                   <div class="col-12" style="margin-bottom: 10px">
@@ -75,19 +88,28 @@ parse_str($_SERVER['QUERY_STRING'], $qs);
                     <input type="text" class="form-control" id="query" name="query" value="<?php echo (isset($_GET['query'])) ? $_GET['query'] : '' ?>" placeholder="Enter search terms">
                   </div>
                   <?php foreach ($db->query('SELECT DISTINCT `key` FROM cv_lesson_meta ORDER BY `key` ASC') as $row) {
+                    if (in_array($row['key'], ['Materials', 'Objectives', 'Excerpt'])) {
+                      continue;
+                    }
                     echo ($query) ? "<div class='col-12'>" : "<div class='col-12 col-sm-6 col-md-4'>";
                     echo "<p style='margin-bottom:0px;margin-top:5px'>{$row['key']}</p>";
-                    $encoded_key = str_replace(' ', '$WS$', $row['key']);
+                    $encoded_key = $row['key'];//str_replace(' ', '$WS$', $row['key']);
                     echo "<select name='{$encoded_key}' class='custom-select'>";
                     $stmt = $db->prepare('SELECT DISTINCT value FROM cv_lesson_meta WHERE `key` = ? ORDER BY value ASC');
                     $stmt->execute([$row['key']]);
                     echo "<option value='all'>All</option>";
                     $isset = isset($_GET[$encoded_key]);
-                    foreach ($stmt->fetchAll() as $row2) {
-                      $encoded_val = str_replace(' ', '$WS$', $row2['value']);
+                    $rows = $stmt->fetchAll();
+                    if ($row['key'] === 'Select grade level(s)') {
+                      usort($rows, 'cmp');
+                    }
+                    foreach ($rows as $row2) {
+                      $encoded_val = $row2['value'];//str_replace(' ', '$WS$', $row2['value']);
                       if ($isset && $_GET[$encoded_key] === $encoded_val) {
+                        $row2['value'] = ucwords($row2['value']);
                         echo "<option value='{$encoded_val}' selected>{$row2['value']}</option>";
                       } else {
+                        $row2['value'] = ucwords($row2['value']);
                         echo "<option value='{$encoded_val}'>{$row2['value']}</option>";
                       }
                     }
@@ -103,14 +125,29 @@ parse_str($_SERVER['QUERY_STRING'], $qs);
         <div class="col-12 col-sm-9">
           <?php $pdf_id = 0;
           foreach ($search_results as $result) {
-            $stmt = $db->prepare('SELECT DISTINCT `key`, value FROM cv_lesson_meta WHERE lesson_id = ? GROUP BY value, `key` ORDER BY `key` ASC, value ASC');
+            $stmt = $db->prepare('SELECT DISTINCT `key`, value FROM cv_lesson_meta WHERE lesson_id = ? AND `key` != "Materials" AND `key` != "Objectives" GROUP BY value, `key` ORDER BY `key` DESC, value ASC');
             $stmt->execute([$result['id']]);
             echo "<div class='card bg-light mb-3'><div class='card-body'><h5 class='card-title'>{$result['title']}</h5>";
             $last_key = -1;
             $rows = $stmt->fetchAll();
             $count = count($rows);
             $buf = '';
+            for ($i=0; $i < $count; $i++) {
+              if ($rows[$i]['key'] === 'View units or lessons') { // print lesson/unit as part of heading
+                $buf = "<h6 class='text-muted text-capitalize'>{$rows[$i]['value']}</h6>";
+                break;
+              }
+            }
+            for ($i=0; $i < $count; $i++) {
+              if ($rows[$i]['key'] === 'Excerpt') { // print excerpt first at top
+                $buf .= "<p style='margin-bottom:2px' class='card-text classpdf{$pdf_id}'>{$rows[$i]['value']}</p>";
+                break;
+              }
+            }
             for ($i=0; $i < $count; $i++) { 
+              if ($rows[$i]['key'] === 'Excerpt' || $rows[$i]['key'] === 'View units or lessons') {
+                continue;
+              }
               if ($last_key !== $rows[$i]['key']) {
                 $buf .= "<p style='margin-bottom:2px' class='card-text classpdf{$pdf_id}'><b>{$rows[$i]['key']}</b>: ";
               }
@@ -123,7 +160,7 @@ parse_str($_SERVER['QUERY_STRING'], $qs);
             echo "{$buf}</p>";
             echo "<embed src='' width='100%' height='600' type='application/pdf' style='display:none;margin-bottom:10px' id='pdf{$pdf_id}'>";
             echo "<p style='margin:15px 0px 0px 0px'><a class='btn btn-primary open-pdf' href='#' data-pdf-url='{$result['pdf']}' data-pdf-id='pdf{$pdf_id}'>Open PDF</a> <a class='btn btn-secondary' href='{$result['pdf']}' download>Download PDF</a></p>";
-            echo "</div><div class='card-footer bg-light'>".date('F jS, Y', strtotime($result['published']))."</div></div>";
+            echo "</div><div class='card-footer bg-light'>".date('F jS, Y', ($result['published']))."</div></div>";
             $pdf_id++;
           }
           if ($pdf_id === 0) {
