2f698c7cfdb40c4518f6a455f770e1373d1306a9
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2018-07-28 17:37:57 -0400

    made search work, changed dropdowns to checkboxes, some formatting stuff

--- edresources/searchedresources.php
+++ edresources/searchedresources.php
@@ -3,23 +3,26 @@ require '../../includes/db.php';
 error_reporting(-1);
 ini_set('display_errors', 'On');
 $params = [];
-if (isset($_GET['submit'])) {
-  $page = (empty($_GET['page'])) ? 0 : intval($_GET['page']) - 1;
+if (isset($_POST['submit'])) {
+  $page = (empty($_POST['page'])) ? 0 : intval($_POST['page']) - 1;
   $limit = 7;
   $offset = $limit * $page;
   $query = true;
   $sql = '';
   $search_query = '';
-  foreach ($_GET as $key => $value) {
+  foreach ($_POST as $key => $value) {
     if ($value === 'all' || $key === 'submit' || $key === 'query' || $key === 'page') {
       if ($key === 'query') {
         $search_query = $value;
       }
       continue;
     }
-    $sql .= "(`key` = ? AND value = ?) OR ";
-    $params[] = $key;//str_replace('$WS$', ' ', $key);
-    $params[] = $value;//str_replace('$WS$', ' ', $value);
+    foreach ($value as $v) {
+      $sql .= "(`key` = ? AND value = ?) AND ";
+      $params[] = str_replace('$WS$', ' ', $key);
+      $params[] = str_replace('$WS$', ' ', $v);
+      // $sql .= "(`key` = '".str_replace('$WS$', ' ', $key)."' AND value = '".str_replace('$WS$', ' ', $v)."') AND ";
+    }
   }
   if ($sql !== '') {
     $sql = 'WHERE ' . substr($sql, 0, -4); // remove the final ' OR '
@@ -30,6 +33,8 @@ if (isset($_GET['submit'])) {
     $params[] = "%{$search_query}%";
     $params[] = "%{$search_query}%";
   }
+  // var_dump($params);
+  // echo "SELECT SQL_CALC_FOUND_ROWS id, title, pdf, published FROM cv_lessons WHERE id IN (SELECT lesson_id FROM cv_lesson_meta {$sql}){$sql2} ORDER BY published DESC LIMIT {$offset}, {$limit}";die;
   $stmt = $db->prepare("SELECT SQL_CALC_FOUND_ROWS id, title, pdf, published FROM cv_lessons WHERE id IN (SELECT lesson_id FROM cv_lesson_meta {$sql}){$sql2} ORDER BY published DESC LIMIT {$offset}, {$limit}");
   $stmt->execute($params);
   $search_results = $stmt->fetchAll();
@@ -52,6 +57,21 @@ function cmp($a, $b) {
   }
   return ($a < $b) ? -1 : 1;
 }
+function format_key($str) {
+  switch ($str) {
+    case 'Select topic(s)':
+      return 'Topic(s)';
+    case 'Select subject(s)':
+      return 'Subject(s)';
+    case 'Select student level(s)':
+      return 'Student level(s)';
+    case 'Select grade level(s)':
+      return 'Grade level(s)';
+    case 'View units or lessons':
+      return 'Type';
+  }
+  return $str;
+}
 ?>
 <!doctype html>
 <html lang="en">
@@ -73,47 +93,54 @@ function cmp($a, $b) {
     <div class="container">
       <?php include '../includes/header.php'; ?>
       <div class="row" style="padding: 30px">
-        <div class="<?php echo($query) ? 'col-12 col-sm-3' : 'col-12' ?>">
+        <div class="<?php echo ($query) ? 'col-12 col-sm-3' : 'col-12' ?>">
           <?php if (!$query) { ?>
           <h1 class="text-center">Search Lessons and Units</h1>
           <p>Educators from a variety of schools have creatively employed Environmental Dashboard to teach a variety of concepts, subjects and levels.  Use the selectors below to search our repository for Dashboard-related lessons and units by title, topic, grade level or any of the other search fields alone or in combination.</p>
           <p>For example, a search for the term "electricity" will take you to titles and summary descriptions of all lessons tagged as being related.  From  this list you can click and download PDFs of complete lessons.  This <a href="https://docs.google.com/document/d/1uJLghYhpDRiZadqeeacH2aUyiedKHI0DhQs_QWsQjLM/edit">Navigation Guide to BuildingOS</a> may be a useful resource as a general introduction to the tools and apps in the BuildingOS component of Dashboard.</p>
           <?php } ?>
-          <form action="" method="GET">
+          <form action="" method="POST">
             <div class="card bg-light mb-3">
               <div class="card-body">
                 <div class="row">
                   <div class="col-12" style="margin-bottom: 10px">
                     <label for="query">Search</label>
-                    <input type="text" class="form-control" id="query" name="query" value="<?php echo (isset($_GET['query'])) ? $_GET['query'] : '' ?>" placeholder="Enter search terms">
+                    <input type="text" class="form-control" id="query" name="query" value="<?php echo (isset($_POST['query'])) ? $_POST['query'] : '' ?>" placeholder="Enter search terms">
                   </div>
-                  <?php foreach ($db->query('SELECT DISTINCT `key` FROM cv_lesson_meta ORDER BY `key` ASC') as $row) {
+                  <?php foreach ($db->query('SELECT DISTINCT `key` FROM cv_lesson_meta ORDER BY `key` ASC') as $i => $row) {
                     if (in_array($row['key'], ['Materials', 'Objectives', 'Excerpt'])) {
                       continue;
                     }
                     echo ($query) ? "<div class='col-12'>" : "<div class='col-12 col-sm-6 col-md-4'>";
                     echo "<p style='margin-bottom:0px;margin-top:5px'>{$row['key']}</p>";
-                    $encoded_key = $row['key'];//str_replace(' ', '$WS$', $row['key']);
-                    echo "<select name='{$encoded_key}' class='custom-select'>";
+                    $encoded_key = str_replace(' ', '$WS$', $row['key']);
+                    echo "<div style='max-height:130px;overflow-y:scroll'>";
                     $stmt = $db->prepare('SELECT DISTINCT value FROM cv_lesson_meta WHERE `key` = ? ORDER BY value ASC');
                     $stmt->execute([$row['key']]);
-                    echo "<option value='all'>All</option>";
-                    $isset = isset($_GET[$encoded_key]);
+                    $isset = isset($_POST[$encoded_key]);
                     $rows = $stmt->fetchAll();
                     if ($row['key'] === 'Select grade level(s)') {
                       usort($rows, 'cmp');
                     }
                     foreach ($rows as $row2) {
-                      $encoded_val = $row2['value'];//str_replace(' ', '$WS$', $row2['value']);
-                      if ($isset && $_GET[$encoded_key] === $encoded_val) {
+                      $uniq = uniqid();
+                      $encoded_val = str_replace(' ', '$WS$', $row2['value']);
+                      if (
+                          ($isset && is_array($_POST[$encoded_key]) && in_array($encoded_val, $_POST[$encoded_key])) || 
+                          ($isset && $_POST[$encoded_key] === $encoded_val)
+                        ) { // checked checkbox
                         $row2['value'] = ucwords($row2['value']);
-                        echo "<option value='{$encoded_val}' selected>{$row2['value']}</option>";
+                        echo "<div class=\"custom-control custom-checkbox\">
+                        <input name=\"{$encoded_key}[]\" type=\"checkbox\" value=\"{$encoded_val}\" class=\"custom-control-input checkbox-btn{$i}\" id=\"{$uniq}\" data-triggeredBy=\"btn{$i}\" data-label=\"{$row2['value']}\" checked=\"checked\" />
+                        <label class=\"custom-control-label\" for=\"{$uniq}\">{$row2['value']}</label></div>";
                       } else {
                         $row2['value'] = ucwords($row2['value']);
-                        echo "<option value='{$encoded_val}'>{$row2['value']}</option>";
+                        echo "<div class=\"custom-control custom-checkbox\">
+                        <input name=\"{$encoded_key}[]\" type=\"checkbox\" value=\"{$encoded_val}\" class=\"custom-control-input checkbox-btn{$i}\" id=\"{$uniq}\" data-triggeredBy=\"btn{$i}\" data-label=\"{$row2['value']}\" />
+                        <label class=\"custom-control-label\" for=\"{$uniq}\">{$row2['value']}</label></div>";
                       }
                     }
-                    echo "</select></div>";
+                    echo " </div></div>";
                   } ?>
                 </div>
               </div>
@@ -125,7 +152,7 @@ function cmp($a, $b) {
         <div class="col-12 col-sm-9">
           <?php $pdf_id = 0;
           foreach ($search_results as $result) {
-            $stmt = $db->prepare('SELECT DISTINCT `key`, value FROM cv_lesson_meta WHERE lesson_id = ? AND `key` != "Materials" AND `key` != "Objectives" GROUP BY value, `key` ORDER BY `key` DESC, value ASC');
+            $stmt = $db->prepare('SELECT DISTINCT `key`, value FROM cv_lesson_meta WHERE lesson_id = ? AND `key` != "Materials" GROUP BY value, `key` ORDER BY `key` DESC, value ASC');
             $stmt->execute([$result['id']]);
             echo "<div class='card bg-light mb-3'><div class='card-body'><h5 class='card-title'>{$result['title']}</h5>";
             $last_key = -1;
@@ -133,23 +160,17 @@ function cmp($a, $b) {
             $count = count($rows);
             $buf = '';
             for ($i=0; $i < $count; $i++) {
-              if ($rows[$i]['key'] === 'View units or lessons') { // print lesson/unit as part of heading
-                $buf = "<h6 class='text-muted text-capitalize'>{$rows[$i]['value']}</h6>";
-                break;
-              }
-            }
-            for ($i=0; $i < $count; $i++) {
               if ($rows[$i]['key'] === 'Excerpt') { // print excerpt first at top
-                $buf .= "<p style='margin-bottom:2px' class='card-text classpdf{$pdf_id}'>{$rows[$i]['value']}</p>";
+                $buf .= "<p style='margin-bottom:2px' class='card-text classpdf{$pdf_id}'><b>{$rows[$i]['key']}</b>: {$rows[$i]['value']}</p>";
                 break;
               }
             }
             for ($i=0; $i < $count; $i++) { 
-              if ($rows[$i]['key'] === 'Excerpt' || $rows[$i]['key'] === 'View units or lessons') {
+              if ($rows[$i]['key'] === 'Excerpt') {
                 continue;
               }
               if ($last_key !== $rows[$i]['key']) {
-                $buf .= "<p style='margin-bottom:2px' class='card-text classpdf{$pdf_id}'><b>{$rows[$i]['key']}</b>: ";
+                $buf .= "<p style='margin-bottom:2px' class='card-text classpdf{$pdf_id}'><b>".format_key($rows[$i]['key'])."</b>: ";
               }
               $buf .= ($i === $count-1) ? "{$rows[$i]['value']}" : "{$rows[$i]['value']}, ";
               if ($i < $count-1 && $rows[$i + 1]['key'] !== $rows[$i]['key']) {
@@ -158,9 +179,9 @@ function cmp($a, $b) {
               $last_key = $rows[$i]['key'];
             }
             echo "{$buf}</p>";
+            $result['pdf'] = trim($result['pdf']);
             echo "<embed src='' width='100%' height='600' type='application/pdf' style='display:none;margin-bottom:10px' id='pdf{$pdf_id}'>";
-            echo "<p style='margin:15px 0px 0px 0px'><a class='btn btn-primary open-pdf' href='#' data-pdf-url='{$result['pdf']}' data-pdf-id='pdf{$pdf_id}'>Open PDF</a> <a class='btn btn-secondary' href='{$result['pdf']}' download>Download PDF</a></p>";
-            echo "</div><div class='card-footer bg-light'>".date('F jS, Y', ($result['published']))."</div></div>";
+            echo "<p style='margin:15px 0px 0px 0px'><a class='btn btn-primary open-pdf' href='#' data-pdf-url='{$result['pdf']}' data-pdf-id='pdf{$pdf_id}'>Open PDF</a> <a class='btn btn-secondary' href='{$result['pdf']}' download>Download PDF</a></p></div></div>";
             $pdf_id++;
           }
           if ($pdf_id === 0) {
@@ -227,6 +248,7 @@ function cmp($a, $b) {
         e.preventDefault();
         $('input:not(input[type="submit"])').val('');
         $('select').val('all');
+        $('input:checkbox').removeAttr('checked');
       });
     </script>
   </body>
