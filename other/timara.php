a384b5287f368bb5d764880d55e905cadcbe1c94
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2018-10-03 23:41:49 -0400

    make this variable too

--- other/timara.php
+++ other/timara.php
@@ -1,8 +1,10 @@
 <?php
 header("Content-type: text/plain");
 require '../../includes/db.php';
-$stmt = $db->prepare('SELECT value, recorded FROM meter_data WHERE meter_id = 346 AND resolution = ? ORDER BY recorded DESC');
-$stmt->execute([(isset($_GET['resolution']) ? $_GET['resolution'] : 'quarterhour')]);
+$stmt = $db->prepare('SELECT value, recorded FROM meter_data WHERE meter_id = ? AND resolution = ? ORDER BY recorded DESC');
+$stmt->execute([
+	(isset($_GET['meter']) ? $_GET['meter'] : 347),
+	(isset($_GET['resolution']) ? $_GET['resolution'] : 'quarterhour')]);
 foreach ($stmt->fetchAll() as $row) {
 	echo implode(',', array_values($row)) . "\n";
 }
