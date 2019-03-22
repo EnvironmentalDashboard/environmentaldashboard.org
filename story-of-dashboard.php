5df9b136f7332e591f2279e07bf97af8a462c878
Olivia Vasquez <ovasquez@ip-80-138.wireless.oberlin.edu>
2019-03-22 11:44:42 -0400

    Added videos

--- story-of-dashboard.php
+++ story-of-dashboard.php
@@ -17,10 +17,97 @@
     <div class="container">
       <?php include 'includes/header.php'; ?>
       <div class='embed-responsive embed-responsive-16by9'><iframe class="embed-responsive-item" src="https://docs.google.com/presentation/d/1AosesvyjnPjp732lvuqF6qgeB8uGh0diTAEyVQuEZBc/embed?start=false&loop=false&delayms=30000" frameborder="0"></iframe></div>
-      <h3 class="text-center">CampusEnergy2017 Student Video Contest: Oberlin College (Like our video!)</h3>
+
+
+     <!--  <h3 class="text-center">CampusEnergy2017 Student Video Contest: Oberlin College (Like our video!)</h3>
       <div class='embed-responsive embed-responsive-16by9'><iframe class="embed-responsive-item" src="https://www.youtube-nocookie.com/embed/1H49cDTKTX0?rel=0" frameborder="0"></iframe></div>
       <h3 class="text-center">Learn more about the Environmental Dashboard from members of the team!</h3>
-      <div class='embed-responsive embed-responsive-16by9'><iframe class="embed-responsive-item" src="https://www.youtube-nocookie.com/embed/eNTC7dDEAsU?rel=0&showinfo=0" frameborder="0"></iframe></div>
+      <div class='embed-responsive embed-responsive-16by9'><iframe class="embed-responsive-item" src="https://www.youtube-nocookie.com/embed/eNTC7dDEAsU?rel=0&showinfo=0" frameborder="0"></iframe></div> -->
+
+      <div class="row text-center">
+      <div class="col-lg-4 col-md-6 mb-4">
+        <div class="card h-100">
+          <iframe width="290" height="200" src="https://www.youtube.com/embed/YSXFKSvN75o" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <div class="card-body">
+            <h4 class="card-title">Climate Change: Global Temperatures</h4>
+            <p class="card-text">What are the trends of global temperature in the past decade?</p>
+          </div>
+          <div class="card-footer">
+            <a href="https://www.youtube.com/embed/YSXFKSvN75o" class="btn btn-primary">Link to Youtube</a>
+          </div>
+        </div>
+      </div>
+
+       <div class="col-lg-4 col-md-6 mb-4">
+        <div class="card h-100">
+          <iframe width="290" height="200" src="https://www.youtube.com/embed/ZFVBPEpA4RA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <div class="card-body">
+            <h4 class="card-title">Technology for Change</h4>
+            <p class="card-text">How does Environmental Dashboard educate, motivate, and empower for a new generation?</p>
+          </div>
+          <div class="card-footer">
+            <a href="https://www.youtube.com/embed/ZFVBPEpA4RA" class="btn btn-primary">Link to Youtube</a>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="col-lg-4 col-md-6 mb-4">
+        <div class="card h-100">
+        <iframe width="290" height="200" src="https://www.youtube.com/embed/2fCQ3qdJnh0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <div class="card-body">
+            <h4 class="card-title">Resources Explained: Carbon Neutrality</h4>
+            <p class="card-text">How much of Oberlin's electricity sourcing is carbon neutral?</p>
+          </div>
+          <div class="card-footer">
+            <a href="https://www.youtube.com/embed/2fCQ3qdJnh0" class="btn btn-primary">Link to Youtube</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6 mb-4">
+        <div class="card h-100">
+        <iframe width="290" height="200" src="https://www.youtube.com/embed/VEUDNRkChXs" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>          
+        <div class="card-body">
+            <h4 class="card-title">Resources Explained: Drinking Water</h4>
+            <p class="card-text">Where does Oberlin get its drinking water from?</p>
+          </div>
+          <div class="card-footer">
+            <a href="https://www.youtube.com/embed/VEUDNRkChXs" class="btn btn-primary">Link to Youtube</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6 mb-4">
+        <div class="card h-100">
+        <iframe width="290" height="200" src="https://www.youtube.com/embed/3zge8P0GtPM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>       
+        <div class="card-body">
+            <h4 class="card-title">Resources Explained: Renewable Energy</h4>
+            <p class="card-text">How does the solar array impact Oberlin's renewable energy?</p>
+          </div>
+          <div class="card-footer">
+            <a href="https://www.youtube.com/embed/3zge8P0GtPM" class="btn btn-primary">Link to Youtube</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6 mb-4">
+        <div class="card h-100">
+        <iframe width="290" height="200" src="https://www.youtube.com/embed/xW_HXfanP68" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>     
+        <div class="card-body">
+            <h4 class="card-title">Oberlin Public Library</h4>
+            <p class="card-text">How do I get to get to the Oberlin Public Library?</p>
+          </div>
+          <div class="card-footer">
+            <a href="https://www.youtube.com/embed/xW_HXfanP68" class="btn btn-primary">Link to Youtube</a>
+          </div>
+        </div>
+      </div>
+
+
+
+
+    </div>
       <div style="clear: both;height: 30px"></div>
       <?php include 'includes/footer.php'; ?>
     </div>
