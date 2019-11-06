a13647c60c329243595fdcee1647c9ce11ad4cb5
Oliver Ripps <oliver.ripps@gmail.com>
2019-11-06 17:02:07 -0500

    working on new press page

---
+++ press-page.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://environmentaldashboard.org/css/bootstrap.css?v=<?php echo time(); ?>">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=9ByOqqx0o3">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=9ByOqqx0o3">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=9ByOqqx0o3">
+    <link rel="manifest" href="/manifest.json?v=9ByOqqx0o3">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg?v=9ByOqqx0o3" color="#00a300">
+    <link rel="shortcut icon" href="/favicon.ico?v=9ByOqqx0o3">
+    <meta name="theme-color" content="#000000">
+    <title>Environmental Dashboard</title>
+  </head>
+  <body>
+    <div class="container">
+      <?php include 'includes/header.php'; ?>
+  <div style="padding: 30px">
+    <h1 class="text-center">Press & Media</h1>
+</div>
+<div class="row text-center">
+  <div class="col-lg-4 col-md-6 mb-4">
+    <div class="card h-100">
+      <a href="pdf"> <img src="" style="width:358px;height:200px">
+      <div class="card-body">
+        <h4 class="card-title">Smart Cities with Urban Dashboards: Giving the Community Feedback About Their City.</h4>
+        <p class="card-text">This article was written on the Environmental Dashboard Displays by the Council Leader magazine of Australia. </p>
+      </div>
+    </div>
+  </div>
+  <div class="col-lg-4 col-md-6 mb-4">
+    <div class="card h-100">
+      <iframe width="358" height="200" src="https://www.youtube.com/watch?v=p-rTQ443akE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <div class="card-body">
+        <h4 class="card-title">2040 Film</h4>
+        <p class="card-text">Environmental Dashboard displays and its use in the classroom are featured in the film 2040. </p>
+      </div>
+      <div class="card-footer">
+        <a href="https://www.youtube.com/watch?v=p-rTQ443akE" class="btn btn-primary">Link to Youtube</a>
+      </div>
+    </div>
+  </div>
+
+
+</div>
+
+      <?php include 'includes/footer.php'; ?>
+    </div>
+    <?php include 'includes/js.php'; ?>
+  </body>
+</html>
