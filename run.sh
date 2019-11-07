4e00766455baacffaaea1c26b979bcae1c31fa90
Sam Craig <craigsamm@gmail.com>
2019-11-06 19:27:43 -0500

    Change the port to 3003
    I use 3002 in development for something else, and would rather not
    reuse ports as often

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run -dit -p 3002:80 --restart always -v /Users/sam/Documents/Programming/EnvironmentalDashboard/environmentaldashboard.org:/var/www/html --name EDORG edorg
+docker run -dit -p 3003:80 --restart always -v /Users/sam/Documents/Programming/EnvironmentalDashboard/environmentaldashboard.org:/var/www/html --name EDORG edorg
