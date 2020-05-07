ca8b26fe8921f5a44c4093c614ebdfd5209fef29
Oliver Ripps <oliver.ripps@gmail.com>
2020-05-07 19:12:32 -0400

    meet the team page finished-waiting on photo configuration from Rowan

--- meet-the-team.php
+++ meet-the-team.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://environmentaldashboard.org/css/bootstrap.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" type="text/css" href="css/mtt.css">
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=9ByOqqx0o3">
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=9ByOqqx0o3">
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=9ByOqqx0o3">
@@ -14,20 +15,142 @@
     <title>Environmental Dashboard</title>
   </head>
   <body>
-    <div class="container">
-      <?php include 'includes/header.php'; ?>
-      <div style="padding: 30px">
-        <h1 class="text-center">Meet the Team</h1>
-        <img class="so-widget-image alignnone" style="max-width: 100%; height: 216px; display: block;" src="https://environmentaldashboard.org/images/uploads/2015/07/John_Petersen-229x300.jpg" alt="" width="165" height="300">
-        <p>John E. Petersen is Project Director and Chair of the Environmental Studies Program at Oberlin College. A systems ecologist by training, Dr. Petersen's research focuses on understanding flows of energy, cycles of material, and feedback control mechanisms operating in environmental and social systems. He has been instrumental in developing the real-time feedback display technologies that power Environmental Dashboard.</p>
-        <img class="so-widget-image alignnone" style="max-width: 100%; height: 208px; display: block;" src="https://environmentaldashboard.org/images/uploads/2015/07/Cindy-Frantz.png" alt="" width="166" height="200">
-        <p>Cindy Frantz is a social and personality psychologist with a strong interest in statistics. Much of her research focuses on humans’ relationship with nature. Her findings suggest that both individuals and the environment benefit when people feel connected to the natural world. She is passionate about the environment and simple living and enjoys gardening, hiking, and practicing yoga.</p>
-        <img class="so-widget-image alignnone" style="max-width: 100%; height: 177px; display: block;" src="https://environmentaldashboard.org/images/uploads/2015/07/Rumi.png" alt="" width="167" height="170">
-        <p>Md Rumi Shammin is Associate Professor of Environmental Studies at Oberlin College and has an interdisciplinary background that combines environmental engineering, natural resources policy and management, and environmental science. His research encompasses topics related to energy-use, climate change policy, life cycle analysis of energy use and carbon emissions in the US, solar home systems in Bangladesh, urban agriculture, energy and behavior, and Genuine Progress Indicator as an alternative indicator for regional planning.</p>
-        <img class="wp-image-2156 alignnone" src="https://environmentaldashboard.org/images/uploads/2015/11/DSC_0149.jpg" alt="DSC_0149" width="167" height="202">
-        <p>Augustus Arthur is the Project Manager of the Environmental Dashboard. He graduated in 2012 with a major in Computer Science from Ashesi University College in Ghana. He briefly worked for Petra Trust private pensions company as an operations analyst. He then went on to pursue a master’s degree in Environmental Education from the University of Minnesota Duluth after which he moved to Ohio to work full time on developing and implementing the Dashboard in Oberlin and in other communities. Augustus grew up in Ghana, West Africa.</p>
-      </div>
-    </div>
-    <?php include 'includes/js.php'; ?>
+    <div class = "container">
+   <h1 class="head">
+      Meet Our Team
+   </h1>
+   <p class="headdes">
+      The Environmental Dashboard has a decade long legacy of being a collaboration between students, faculty
+      , community members, and employees. Hover for more information and to see what makes the Dashboard team unique.
+   </p>
+   <h1 class="head">
+     Primary Staff
+   </h1>
+   <div class="toprow">
+   <div class="flip-card">
+<div class="flip-card-inner">
+ <div class="flip-card-front">
+   <img src="/images/" alt="John">
+ </div>
+ <div class="flip-card-back">
+   <p class="name">John Petersen</p>
+   <p class="description">Director</p>
+   <p class="description">Was in a Russian dancing troop in high school</p>
+ </div>
+</div>
+</div>
+<div class="flip-card">
+<div class="flip-card-inner">
+<div class="flip-card-front">
+<img src="/images/" alt="Avatar" style="width:300px;height:300px;">
+</div>
+<div class="flip-card-back">
+  <p class="name">Rowan Hannan</p>
+  <p class="description">Project Manager</p>
+  <p class="description">Collects and pins insects as a hobby </p>
+</div>
+</div>
+</div>
+</div>
+<br>
+<h1 class="head">
+   Student Team
+</h1>
+<div class="row">
+  <div class="flip-card">
+  <div class="flip-card-inner">
+  <div class="flip-card-front">
+  <img src="/images/" alt="Avatar" style="width:300px;height:300px;">
+  </div>
+  <div class="flip-card-back">
+    <p class="name">Sam Craig</p>
+    <p class="description">Senior Programmer</p>
+    <p class="description">Won his age group at the Chicago half marathon. They mailed me the award for the wrong Sam</p>
+  </div>
+  </div>
+  </div>
+  <div class="flip-card">
+  <div class="flip-card-inner">
+  <div class="flip-card-front">
+  <img src="/images/" alt="Avatar" style="width:300px;height:300px;">
+  </div>
+  <div class="flip-card-back">
+    <p class="name">Phoebe von Conta</p>
+    <p class="description">CV Manager</p>
+    <p class="description">On the Cross Country and Track team</p>
+  </div>
+  </div>
+  </div>
+  <div class="flip-card">
+  <div class="flip-card-inner">
+  <div class="flip-card-front">
+  <img src="/images/" alt="Avatar" style="width:300px;height:300px;">
+  </div>
+  <div class="flip-card-back">
+    <p class="name">Harriet McSurdy</p>
+    <p class="description">Social Media/Research</p>
+    <p class="description">Favorite color is orange</p>
+  </div>
+  </div>
+  </div>
+</div>
+<br>
+<div class="row">
+  <div class="flip-card">
+  <div class="flip-card-inner">
+  <div class="flip-card-front">
+  <img src="/images/" alt="Avatar" style="width:300px;height:300px;">
+  </div>
+  <div class="flip-card-back">
+    <p class="name">Oliver Ripps</p>
+    <p class="description">Programmer</p>
+    <p class="description">Is Tik Tok Famous</p>
+  </div>
+  </div>
+  </div>
+  <div class="flip-card">
+  <div class="flip-card-inner">
+  <div class="flip-card-front">
+  <img src="/images/" alt="Avatar" style="width:300px;height:300px;">
+  </div>
+  <div class="flip-card-back">
+    <p class="name">Madeleine Faubert</p>
+    <p class="description">Design and media</p>
+    <p class="description">I'm a cult baby</p>
+  </div>
+  </div>
+  </div>
+  <div class="flip-card">
+  <div class="flip-card-inner">
+  <div class="flip-card-front">
+  <img src="/images/" alt="Avatar" style="width:300px;height:300px;">
+  </div>
+  <div class="flip-card-back">
+    <p class="name">Andrew Crapitto</p>
+    <p class="description">Programmer</p>
+    <p class="description">Isn't very fun</p>
+  </div>
+  </div>
+  </div>
+</div>
+<br>
+<div class="lastrow">
+  <div class="flip-card">
+  <div class="flip-card-inner">
+  <div class="flip-card-front">
+    <p class="name">Interested in joining our team?</p>
+    <p class="description">Email Dashboard@oberlin.edu</p>
+    <p class="description">For more info</p>
+  </div>
+  <div class="flip-card-back">
+    <p class="name">Interested in joining our team?</p>
+    <p class="description">Email Dashboard@oberlin.edu</p>
+    <p class="description">For more info</p>
+  </div>
+  </div>
+  </div>
+  <br>
+</div>
+  <?php include 'includes/js.php'; ?>
   </body>
-</html>php
+</html>
