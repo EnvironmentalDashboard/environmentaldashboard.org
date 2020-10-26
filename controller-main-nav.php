4e3604705e0a3c36fdab1c8246d1c7cda387df78
Oliver Ripps <oliver.ripps@gmail.com>
2020-10-26 11:05:16 -0400

    first version of each page without the correct images and links inserted

---
+++ controller-main-nav.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" type="text/css" href="css/cmn.css">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=9ByOqqx0o3">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=9ByOqqx0o3">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=9ByOqqx0o3">
+    <link rel="manifest" href="/manifest.json?v=9ByOqqx0o3">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg?v=9ByOqqx0o3" color="#00a300">
+    <link rel="shortcut icon" href="/favicon.ico?v=9ByOqqx0o3">
+    <meta name="theme-color" content="#000000">
+    <title>Oberlin Hub</title>
+  </head>
+  <body>
+    <div class="container">
+      <div class="topline">
+        OBERLIN HUB
+      </div>
+      <a class="subheader" href="https://environmentaldashboard.org/calendar">
+        <div class="subflex">
+          <img class="newsletter" src="/images/sampleemailimage.png">
+          <p class="captionline"> CLICK HERE TO SUBSCRIBE TO OUR EVENTS NEWSLETTER<p>
+          </div>
+      </a>
+      <div class="controller">
+        <div class="col">
+          <a href="https://environmentaldashboard.org/calendar"> <img class="buttons" src=""> </a>
+          <p class="below">Calendar</p>
+        </div>
+        <div class="col">
+          <a href="https://environmentaldashboard.org/cwd"> <img class="buttons" src=""> </a>
+          <p class="below">Citywide Dashboard</p>
+        </div>
+      </div>
+      <div class="controller">
+        <div class="col">
+          <a href="remotes-page.php "> <img class="buttons" src=""> </a>
+          <p class="below">Remotes</p>
+        </div>
+        <div class="col">
+          <a href="https://environmentaldashboard.org/cwd"> <img class="buttons" src=""> </a>
+          <p class="below">CV</p>
+        </div>
+      </div>
+    </div>
+    <?php include 'includes/js.php'; ?>
+  </body>
+</html>
---
+++ remotes-page.php
@@ -0,0 +1,71 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" type="text/css" href="css/rp.css">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=9ByOqqx0o3">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=9ByOqqx0o3">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=9ByOqqx0o3">
+    <link rel="manifest" href="/manifest.json?v=9ByOqqx0o3">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg?v=9ByOqqx0o3" color="#00a300">
+    <link rel="shortcut icon" href="/favicon.ico?v=9ByOqqx0o3">
+    <meta name="theme-color" content="#000000">
+    <title>Oberlin Remotes</title>
+  </head>
+  <body>
+    <div class="container">
+      <div class="header">
+        OBERLIN REMOTES
+      </div>
+      <div class="subhead">
+        <p> CHOOSE A REMOTE BELOW TO CONTROL A DASHBOARD AROUND TOWN</p>
+      </div>
+      <div class="controller">
+        <a class="button" href="https://environmentaldashboard.org/calendar">
+          <div class="trycircle">
+            SLOW TRAIN
+          </div>
+        </a>
+      </div>
+      <div class="controller">
+        <a class="button" href="https://environmentaldashboard.org/calendar">
+          <div class="onelinecircle">
+            IGA
+          </div>
+        </a>
+      </div>
+      <div class="controller">
+        <a class="button" href="https://environmentaldashboard.org/calendar">
+          <div class="onelinecircle">
+            COWORK OB.
+          </div>
+        </a>
+      </div>
+      <div class="controller">
+        <a class="button" href="https://environmentaldashboard.org/calendar">
+          <div class="onelinecircle">
+            HOTEL
+          </div>
+        </a>
+      </div>
+      <div class="controller">
+        <a class="button" href="https://environmentaldashboard.org/calendar">
+          <div class="onelinecircle">
+            THE BRIDGE
+          </div>
+        </a>
+      </div>
+      <div class="controller">
+        <a class="button" href="https://environmentaldashboard.org/calendar">
+          <div class="multilinecircle">
+            <p class="circletext">
+              OBERLIN PUBLIC LIBRARY
+            </p>
+          </div>
+        </a>
+      </div>
+    </div>
+    <?php include 'includes/js.php'; ?>
+  </body>
+</html>
