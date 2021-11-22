8f4aed1211c7ecb137ba5ae982daf5deae407c57
Caleb-Stripes <calebdstripes@gmail.com>
2021-11-22 08:56:14 -0800

    controller carousel needs to advance frames but looks good

--- nav/scripts/ajlc-advanced-controller.js
+++ nav/scripts/ajlc-advanced-controller.js
@@ -1,41 +1,45 @@
 //This file contains the scripts used to make the ajlc controller interactive, it does not contain the scripts that link the buttons to digital signage.
-const btnOne = document.getElementById('menuBtn1')
-    
-btnOne.addEventListener('click', openOne);
 
 //function to open a carousel
-function openOne() {
-    if (panelOne.classList == 'panel open'){
-        toggleOff();    
-    } else {
-        toggleOff();
-        panelOne.classList.toggle('open')
-        panelTwo.classList.toggle('close')
-        panelThree.classList.toggle('close')
-        descriptions[0].style.visibility = "visible";
-    }
-}
 
 //function to close the carousels
-function toggleOff() {
-    const panels = [panelOne, panelTwo, panelThree]
-    panels.forEach(panel => {
-        panel.classList = 'panel';
-    });
-    descriptions[0].style.visibility = "hidden";
-    descriptions[1].style.visibility = "hidden";
-    descriptions[2].style.visibility = "hidden";
-}
+
 
 //function to navigate carousel with spots eventurally this will need to be adapted for swiping on an iPad
-const spotOne = document.getElementById('spotOne')
-const spotTwo = document.getElementById('spotTwo')
-const spotThree = document.getElementById('spotThree')
-
-var advance = function () {
-    console.log ('working')
-}
-
-spotOne.addEventListener('click', advance)
-spotTwo.addEventListener('click', advance)
-spotThree.addEventListener('click', advance)
+document.querySelectorAll(".carousel").forEach((carousel) => {
+    const items = carousel.querySelectorAll(".carousel_item");
+    const buttonsHtml = Array.from(items, () => {
+      return `<span class="carousel_button"></span>`;
+    });
+    console.table(buttonsHtml)
+  
+    carousel.insertAdjacentHTML(
+      "beforeend",
+      `
+          <div class="carousel_nav">
+              ${buttonsHtml.join("")}
+          </div>
+      `
+    );
+  
+    const buttons = carousel.querySelectorAll(".carousel_button");
+  
+    buttons.forEach((button, i) => {
+      button.addEventListener("click", () => {
+        // un-select all the items
+        items.forEach((item) =>
+          item.classList.remove("carousel_item--selected")
+        );
+        buttons.forEach((button) =>
+          button.classList.remove("carousel_button--selected")
+        );
+  
+        items[i].classList.add("carousel_item--selected");
+        button.classList.add("carousel_button--selected");
+      });
+      // Select the first item on page load
+      items[0].classList.add("carousel_item--selected");
+      buttons[0].classList.add("carousel_button--selected");
+    });
+  
+  });
