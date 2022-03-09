db2c8b7cd583b752c730dab4b7467f0e76159743
Caleb Stripes <59482037+Caleb-Stripes@users.noreply.github.com>
2022-03-09 07:46:20 -0500

    Update index.js

--- nav/random-content/index.js
+++ nav/random-content/index.js
@@ -3,37 +3,45 @@
   
 // return items[Math.floor(Math.random()*items.length)];
      
-// }
-
-// var items = [254, 45, 212, 365, 2543];
-// console.log(random_item(items));
-var iFrameSource = document.getElementById('zero').src;
+// Array of src urls from pool of content
 const srcArray = [];
 srcArray[0] = 'https://docs.google.com/presentation/d/1C9R1uJ3UxTYtFIvq5gJJqNN4mo3kpLpk-dIRYZLBVXc/preview?rm=minimal&slide=id.g1163cbbd3f2_0_0',
 srcArray[1] = 'https://docs.google.com/presentation/d/1C9R1uJ3UxTYtFIvq5gJJqNN4mo3kpLpk-dIRYZLBVXc/preview?rm=minimal&slide=id.g1163cbbd3f2_0_254',
-srcArray[2] = 'https://docs.google.com/presentation/d/1C9R1uJ3UxTYtFIvq5gJJqNN4mo3kpLpk-dIRYZLBVXc/preview?rm=minimal&slide=id.g1163cbbd3f2_0_5'
+srcArray[2] = 'https://docs.google.com/presentation/d/1C9R1uJ3UxTYtFIvq5gJJqNN4mo3kpLpk-dIRYZLBVXc/preview?rm=minimal&slide=id.g1163cbbd3f2_0_5',
+srcArray[3] = 'https://docs.google.com/presentation/d/1C9R1uJ3UxTYtFIvq5gJJqNN4mo3kpLpk-dIRYZLBVXc/preview?rm=minimal&slide=id.g1163cbbd3f2_2_0',
+srcArray[4] = 'https://docs.google.com/presentation/d/1C9R1uJ3UxTYtFIvq5gJJqNN4mo3kpLpk-dIRYZLBVXc/preview?rm=minimal&slide=id.g11435c5e347_0_0',
+srcArray[5] = 'https://docs.google.com/presentation/d/1C9R1uJ3UxTYtFIvq5gJJqNN4mo3kpLpk-dIRYZLBVXc/preview?rm=minimal&slide=id.g11435c5e347_0_4'
 
-//function to grab random array url
-function myFunction(items) {
-    //console.log("hello"); 1
-    //console.log(iFrameSource); 2
-    return items[Math.floor(Math.random()*items.length)];
-}
-
-//console.log(myFunction(srcArray)); 3
-function srcSwap(){
-    //variable to store the new src
-    var newId = myFunction(srcArray);
-    //variable to store the current id
-    var currentId = document.getElementById('zero').src;
-    //check to make sure the new id is not the same as the current before updating the src
-    if(newId == currentId) {
-        return srcSwap()
-    } else {
-        document.getElementById('zero').src = newId;
+function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
     }
+  
+    return array;
+  }
 
-}
+  function srcSwap(){
+      //check if the array is empty and refresh it
+      if (srcArray.length <= 1) {
+          location.reload();
+      }
 
-//runs the shuffle constantly, works just comment out for development
-setInterval(srcSwap, 3000);
+      //shuffle sources
+      shuffle(srcArray);
+
+      //take the last one for the iFrame src
+      document.getElementById('zero').src = srcArray.pop();
+  }
+
+  srcSwap();
+  setInterval(srcSwap, 8000);
