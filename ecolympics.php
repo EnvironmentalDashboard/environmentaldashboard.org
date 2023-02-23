7f4db65cba837516b79243a090ab58be1018d03a
Hitesh Jangid <hiteshjangid19@gmail.com>
2023-02-23 18:43:15 +0530

    add ecolympic page

---
+++ ecolympics.php
@@ -0,0 +1,105 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description"
+        content="Environmental Dashboard. A technology & approach for organizations and whole communities that combines feedback, through real-time public displays of resource use and environmental conditions, with thoughts and actions of community to engage, motivate, empower & celebrate sustainable thought and action.">
+    <link rel="stylesheet" href="https://environmentaldashboard.org/css/bootstrap.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="/css/ecolympics.css?v=<?php echo time(); ?>">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=9ByOqqx0o3">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=9ByOqqx0o3">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=9ByOqqx0o3">
+    <link rel="manifest" href="/manifest.json?v=9ByOqqx0o3">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg?v=9ByOqqx0o3" color="#00a300">
+    <link rel="shortcut icon" href="/favicon.ico?v=9ByOqqx0o3">
+    <meta name="theme-color" content="#000000">
+    <title>Environmental Dashboard</title>
+</head>
+
+<body>
+    <div class="container">
+        <?php include 'includes/header.php'; ?>
+        <div class="more-info-links">
+            <a href="https://buildingos.com/s/oberlincity/storyboard314/?chapterId=1382" target="_blank"
+                title="Oberlin City Schools">
+                <div class="link">
+                    <img src="../images/ecolympics/OPSIcon.svg" alt="" srcset="">
+                    <span>Oberlin City Schools</span>
+                </div>
+            </a>
+            <a href="https://buildingos.com/s/oberlincity/storyboard31413736/?chapterId=77277" target="_blank"
+                title="City Of Oberlin">
+                <div class="link">
+                    <img src="../images/ecolympics/CofOberlinIcon.svg" alt="City of Oberlin" srcset="">
+                    <span>City Of Oberlin</span>
+                </div>
+            </a>
+            <a href="#" target="_blank"
+                title="Oberlin College">
+                <div class="link">
+                    <img src="../images/ecolympics/OClogo.svg" alt="" srcset="">
+                    <span>Oberlin College</span>
+                </div>
+            </a>
+        </div>
+        <div class="rank-data row">
+            <div class="col-md-6 pr-0">
+                <div class="col-md-12 pr-0">
+                    <iframe src="https://buildingos.com/blocks/0c64841f267c4efd95d450db6727ab34/"
+                        allowtransparency="true" frameborder="0" width="520" height="420"></iframe>
+                </div>
+                <div class="col-md-12 pr-0">
+                    <iframe src="https://buildingos.com/blocks/071a0710a0a3435d8a08e9340a16e6bf/"
+                        allowtransparency="true" frameBorder=0 width="520" height="420"></iframe>
+                </div>
+                <div class="col-md-12 pr-0">
+                    <iframe src="https://buildingos.com/blocks/d6105e422c9048fc816758193ca84b4f/"
+                        allowtransparency="true" frameBorder=0 width="520" height="1200"></iframe>
+                </div>
+            </div>
+            <div class="col-md-6 pl-0">
+                <div class="col-md-12 pl-0">
+                    <h1>WHAT IS ECOLYMPICS</h1>
+                    <P>
+                        Ecolympics is the Oberlin community’s annual resource reduction competition focused on the
+                        decrease of water and electricity use in participating buildings through behavior change.
+                        Environmental Dashboard provides the means to enact those changes by making resource flows
+                        visible and accessible. Use the dashboard to view electricity and water use in real-time.
+                        This
+                        year’s Ecolympics theme is to Act Local, Think Globally.
+                    </P>
+                    <P>
+                        The 2023 Ecolympics will feature three competitions: dormitories on Oberlin College’s campus
+                        will compete. Oberlin Elementary School, Langston Middle School, and Oberlin High School
+                        will
+                        compete. Public buildings including the Prospect Community Center, Oberlin Fire Station,
+                        OCSD
+                        Board of Education office, and Oberlin Public Library will also compete. While participants
+                        are
+                        eager to win, everyone should work towards the community-wide goal to reduce electricity
+                        consumption by 10,000 kWh.
+                    </P>
+                    <P>
+                        Above/below are the current standings for competitions as well as the community-wide
+                        progress.
+                        To see in-depth data and information for a specific building, click on the live data button.
+                        To
+                        see where your electricity and water come from, as well as current environmental conditions,
+                        check out
+                    </P>
+                </div>
+                <div class="col-md-12 pl-0">
+                    <h1>COMMUNITY VOICES</h1>
+                    <iframe src="http://oberlin.communityhub.cloud/calendar/ecolympic"
+                        allowtransparency="true" scrolling ="no"  frameBorder=0 width="100%" height="470px"></iframe>
+                </div>
+            </div>
+        </div>
+        <?php include 'includes/footer.php'; ?>
+    </div>
+    <?php include 'includes/js.php'; ?>
+</body>
+
+</html>
