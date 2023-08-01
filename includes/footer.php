413de331621f7d981b092c29b2cd4db144fb00b7
Hitesh Jangid <hiteshjangid19@gmail.com>
2023-08-01 13:16:27 +0530

    set Collagborator's link in footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,31 +1,49 @@
 <div class="row text-center justify-content-md-center">
   <div class="col-3 col-md-1">
-    <img src="https://environmentaldashboard.org/images/uploads/2015/08/sf1-300x227.jpg" alt="" class="img-fluid grow">
+    <a href="https://www.statefarm.com/" target="_blank" title="State Farm">
+      <img src="https://environmentaldashboard.org/images/uploads/2015/08/sf1-300x227.jpg" alt="" class="img-fluid grow">
+    </a>
   </div>
   <div class="col-3 col-md-1">
-    <img src="https://environmentaldashboard.org/images/uploads/2015/08/op1-300x162.jpg" alt="" class="img-fluid grow">
+    <a href="https://oberlinproject.org/" target="_blank" title="The Oberlin Project'">
+      <img src="https://environmentaldashboard.org/images/uploads/2015/08/op1-300x162.jpg" alt="" class="img-fluid grow">
+    </a>
   </div>
   <div class="col-3 col-md-1">
-    <img src="https://environmentaldashboard.org/images/uploads/2015/08/glpf1-297x300.jpg" alt="" class="img-fluid grow">
+    <a href="https://glpf.org/" target="_blank" title="Great Lakes Protection Fund">
+      <img src="https://environmentaldashboard.org/images/uploads/2015/08/glpf1-297x300.jpg" alt="" class="img-fluid grow">
+    </a>
   </div>
   <div class="col-3 col-md-1">
-    <img src="https://environmentaldashboard.org/images/uploads/2015/07/ob-300x300.jpg" alt="" class="img-fluid grow">
+    <a href="https://cityofoberlin.com/" target="_blank" title="City of Oberlin">
+      <img src="https://environmentaldashboard.org/images/uploads/2015/07/ob-300x300.jpg" alt="" class="img-fluid grow">
+    </a>
   </div>
   <div class="col-3 col-md-1">
-    <img src="https://environmentaldashboard.org/images/uploads/2015/07/epa-300x300.jpg" alt="" class="img-fluid grow">
+    <a href="https://www.epa.gov/" target="_blank" title="U.S. Environmental Protection Agency">
+      <img src="https://environmentaldashboard.org/images/uploads/2015/07/epa-300x300.jpg" alt="" class="img-fluid grow">
+    </a>
   </div>
   <div class="col-3 col-md-1">
-    <img src="https://environmentaldashboard.org/images/uploads/2015/07/glca-300x300.jpg" alt="" class="img-fluid grow">
+    <a href="https://www.glca.org/" target="_blank" title="Great Lakes Colleges Association">
+      <img src="https://environmentaldashboard.org/images/uploads/2015/07/glca-300x300.jpg" alt="" class="img-fluid grow">
+    </a>
   </div>
   <div class="col-3 col-md-1">
-    <img src="https://environmentaldashboard.org/images/uploads/2015/08/lucid-300x252.jpg" alt="" class="img-fluid grow">
+    <a href="" target="_blank" title="">
+      <img src="https://environmentaldashboard.org/images/uploads/2015/08/lucid-300x252.jpg" alt="" class="img-fluid grow">
+    </a>
   </div>
   <div class="col-3 col-md-1">
-    <img src="https://environmentaldashboard.org/images/uploads/2015/07/oc-300x300.jpg" alt="" class="img-fluid grow">
+    <a href="https://www.oberlin.edu/" target="_blank" title="Oberlin College and Conservatory">
+      <img src="https://environmentaldashboard.org/images/uploads/2015/07/oc-300x300.jpg" alt="" class="img-fluid grow">
+    </a>
   </div>
 </div>
 <div class="row">
-  <div class="col"><p class="text-muted text-center" style="margin-top: 30px">Oberlin College <?php echo date('Y') ?> | <a href="mailto:dashboard@oberlin.edu" style="color: #6c757d; text-decoration: underline;">Contact Us</a></p></div>
+  <div class="col">
+    <p class="text-muted text-center" style="margin-top: 30px">Oberlin College <?php echo date('Y') ?> | <a href="mailto:dashboard@oberlin.edu" style="color: #6c757d; text-decoration: underline;">Contact Us</a></p>
+  </div>
 </div>
 <div class="row">
   <div class="col text-center" style="padding-bottom: 50px">
