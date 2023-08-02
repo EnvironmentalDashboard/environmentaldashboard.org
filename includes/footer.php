bf6036c23cbdcfb17b9186fc0e63a54520f8a524
Hitesh Jangid <hiteshjangid19@gmail.com>
2023-08-02 14:41:53 +0530

    adjust css for footer item. hide lucid lgo

--- includes/footer.php
+++ includes/footer.php
@@ -1,54 +1,61 @@
-<div class="row text-center justify-content-md-center">
+<div class="row justify-content-md-center align-items-center">
   <div class="col-3 col-md-1">
-    <a href="https://www.statefarm.com/" target="_blank" title="State Farm">
+    <a href="https://www.statefarm.com/" target="_blank" title="State Farm" class="d-inline-block">
       <img src="https://environmentaldashboard.org/images/uploads/2015/08/sf1-300x227.jpg" alt="" class="img-fluid grow">
     </a>
   </div>
   <div class="col-3 col-md-1">
-    <a href="https://oberlinproject.org/" target="_blank" title="The Oberlin Project'">
+    <a href="https://oberlinproject.org/" target="_blank" title="The Oberlin Project'" class="d-inline-block">
       <img src="https://environmentaldashboard.org/images/uploads/2015/08/op1-300x162.jpg" alt="" class="img-fluid grow">
     </a>
   </div>
   <div class="col-3 col-md-1">
-    <a href="https://glpf.org/" target="_blank" title="Great Lakes Protection Fund">
+    <a href="https://glpf.org/" target="_blank" title="Great Lakes Protection Fund" class="d-inline-block">
       <img src="https://environmentaldashboard.org/images/uploads/2015/08/glpf1-297x300.jpg" alt="" class="img-fluid grow">
     </a>
   </div>
   <div class="col-3 col-md-1">
-    <a href="https://cityofoberlin.com/" target="_blank" title="City of Oberlin">
+    <a href="https://cityofoberlin.com/" target="_blank" title="City of Oberlin" class="d-inline-block">
       <img src="https://environmentaldashboard.org/images/uploads/2015/07/ob-300x300.jpg" alt="" class="img-fluid grow">
     </a>
   </div>
   <div class="col-3 col-md-1">
-    <a href="https://www.epa.gov/" target="_blank" title="U.S. Environmental Protection Agency">
+    <a href="https://www.epa.gov/" target="_blank" title="U.S. Environmental Protection Agency" class="d-inline-block">
       <img src="https://environmentaldashboard.org/images/uploads/2015/07/epa-300x300.jpg" alt="" class="img-fluid grow">
     </a>
   </div>
   <div class="col-3 col-md-1">
-    <a href="https://www.glca.org/" target="_blank" title="Great Lakes Colleges Association">
+    <a href="https://www.glca.org/" target="_blank" title="Great Lakes Colleges Association" class="d-inline-block">
       <img src="https://environmentaldashboard.org/images/uploads/2015/07/glca-300x300.jpg" alt="" class="img-fluid grow">
     </a>
   </div>
-  <div class="col-3 col-md-1">
-    <a href="" target="_blank" title="">
+  <!-- <div class="col-3 col-md-1">
+    <a href="" target="_blank" title="" class="d-inline-block">
       <img src="https://environmentaldashboard.org/images/uploads/2015/08/lucid-300x252.jpg" alt="" class="img-fluid grow">
     </a>
-  </div>
+  </div> -->
   <div class="col-3 col-md-1">
-    <a href="https://www.oberlin.edu/" target="_blank" title="Oberlin College and Conservatory">
+    <a href="https://www.oberlin.edu/" target="_blank" title="Oberlin College and Conservatory" class="d-inline-block">
       <img src="https://environmentaldashboard.org/images/uploads/2015/07/oc-300x300.jpg" alt="" class="img-fluid grow">
     </a>
   </div>
 </div>
 <div class="row">
   <div class="col">
-    <p class="text-muted text-center" style="margin-top: 30px">Oberlin College <?php echo date('Y') ?> | <a href="mailto:dashboard@oberlin.edu" style="color: #6c757d; text-decoration: underline;">Contact Us</a></p>
+    <p class="text-muted text-center" style="margin-top: 30px">Oberlin College <?php echo date('Y') ?> | <a href="mailto:dashboard@oberlin.edu" style="color: #6c757d; text-decoration: underline;">dashboard@oberlin.edu</a>
+    </p>
   </div>
 </div>
 <div class="row">
-  <div class="col text-center" style="padding-bottom: 50px">
-    <a href="https://www.facebook.com/oberlindashboard/" class="btn btn-primary" style="height: 35px;width: 35px;padding: 5px"><img src="https://environmentaldashboard.org/images/facebook-f.svg" alt="Environmental Dashboard on Facebook" style="height: 100%"></a>
-    <a href="https://twitter.com/envirodashboard" class="btn btn-primary" style="height: 35px;width: 35px;padding: 5px"><img src="https://environmentaldashboard.org/images/twitter.svg" style="height: 100%" alt="Environmental Dashboard on Twitter"></a>
-    <a href="https://www.instagram.com/environmentaldashboard_project/" class="btn btn-primary" style="height: 35px;width: 35px;padding: 5px"><img src="https://environmentaldashboard.org/images/instagram.svg" style="height: 100%" alt="Environmental Dashboard on Instagram"></a>
+  <div class="col text-center d-flex justify-content-center align-items-center" style="padding-bottom: 50px; gap: 0.75rem;">
+    <a href="https://www.facebook.com/oberlindashboard/" class="btn btn-primary d-flex" style="height: 35px;width: 35px;padding: 5px">
+      <img src="https://environmentaldashboard.org/images/facebook-f.svg" alt="Environmental Dashboard on Facebook" style="height: 100%">
+    </a>
+    <a href="https://twitter.com/envirodashboard" class="btn btn-primary d-flex" style="height: 35px;width: 35px;padding: 5px">
+      <img src="https://environmentaldashboard.org/images/twitter.svg" style="height: 100%" alt="Environmental Dashboard on Twitter">
+    </a>
+    <a href="https://www.instagram.com/environmentaldashboard_project/" class="btn btn-primary d-flex" style="height: 35px;width: 35px;padding: 5px">
+      <img src="https://environmentaldashboard.org/images/instagram.svg" style="height: 100%" alt="Environmental Dashboard on Instagram">
+    </a>
   </div>
 </div>
