de104abd0063b27856bceafbae16288c993b0117
Hitesh Jangid <hiteshjangid19@gmail.com>
2024-01-28 20:14:08 +0530

    align promoter content

--- includes/footer.php
+++ includes/footer.php
@@ -1,40 +1,40 @@
-<div class="row justify-content-md-center align-items-center">
-  <div class="col-3 col-md-1">
+<div class="promoters-container">
+  <div class="promotors">
     <a href="https://www.statefarm.com/" target="_blank" title="State Farm" class="d-inline-block">
       <img src="https://environmentaldashboard.org/images/uploads/2015/08/sf1-300x227.jpg" alt="" class="img-fluid grow">
     </a>
   </div>
-  <div class="col-3 col-md-1">
+  <div class="promotors">
     <a href="https://oberlinproject.org/" target="_blank" title="The Oberlin Project'" class="d-inline-block">
       <img src="https://environmentaldashboard.org/images/uploads/2015/08/op1-300x162.jpg" alt="" class="img-fluid grow">
     </a>
   </div>
-  <div class="col-3 col-md-1">
+  <div class="promotors">
     <a href="https://glpf.org/" target="_blank" title="Great Lakes Protection Fund" class="d-inline-block">
       <img src="https://environmentaldashboard.org/images/uploads/2015/08/glpf1-297x300.jpg" alt="" class="img-fluid grow">
     </a>
   </div>
-  <div class="col-3 col-md-1">
+  <div class="promotors">
     <a href="https://cityofoberlin.com/" target="_blank" title="City of Oberlin" class="d-inline-block">
       <img src="https://environmentaldashboard.org/images/uploads/2015/07/ob-300x300.jpg" alt="" class="img-fluid grow">
     </a>
   </div>
-  <div class="col-3 col-md-1">
+  <div class="promotors">
     <a href="https://www.epa.gov/" target="_blank" title="U.S. Environmental Protection Agency" class="d-inline-block">
       <img src="https://environmentaldashboard.org/images/uploads/2015/07/epa-300x300.jpg" alt="" class="img-fluid grow">
     </a>
   </div>
-  <div class="col-3 col-md-1">
+  <div class="promotors">
     <a href="https://www.glca.org/" target="_blank" title="Great Lakes Colleges Association" class="d-inline-block">
       <img src="https://environmentaldashboard.org/images/uploads/2015/07/glca-300x300.jpg" alt="" class="img-fluid grow">
     </a>
   </div>
-  <!-- <div class="col-3 col-md-1">
+  <!-- <div class="promotors">
     <a href="" target="_blank" title="" class="d-inline-block">
       <img src="https://environmentaldashboard.org/images/uploads/2015/08/lucid-300x252.jpg" alt="" class="img-fluid grow">
     </a>
   </div> -->
-  <div class="col-3 col-md-1">
+  <div class="promotors">
     <a href="https://www.oberlin.edu/" target="_blank" title="Oberlin College and Conservatory" class="d-inline-block">
       <img src="https://environmentaldashboard.org/images/uploads/2015/07/oc-300x300.jpg" alt="" class="img-fluid grow">
     </a>
@@ -63,3 +63,17 @@
     </a>
   </div>
 </div>
+<style>
+  .promoters-container {
+    display: flex;
+    gap: max(1vw, 15px);
+    align-items: center;
+    flex-flow: wrap;
+    justify-content: center;
+    /* row justify-content-md-center align-items-center */
+  }
+
+  .promoters-container .promotors {
+    max-width: 70px;
+  }
+</style>
