0693e29a8113ea93e68a96611843dfd448c0a0b7
Hitesh Jangid <hiteshjangid19@gmail.com>
2024-02-15 01:59:32 +0530

    create separate page for social media link & include in footer

--- includes/footer.php
+++ includes/footer.php
@@ -48,19 +48,8 @@
 </div>
 
 <div class="row">
-  <div class="col text-center d-flex justify-content-center align-items-center social-btn-container">
-    <a href="https://www.facebook.com/oberlindashboard/" class="btn btn-primary d-flex social-btn">
-      <img src="https://environmentaldashboard.org/images/facebook-f.svg" alt="Environmental Dashboard on Facebook">
-    </a>
-    <a href="https://twitter.com/EnvDash" class="btn btn-primary d-flex social-btn">
-      <img src="https://environmentaldashboard.org/images/twitter.svg" alt="Environmental Dashboard on Twitter">
-    </a>
-    <a href="https://www.instagram.com/environmentaldashboard/" class="btn btn-primary d-flex social-btn">
-      <img src="https://environmentaldashboard.org/images/instagram.svg" alt="Environmental Dashboard on Instagram">
-    </a>
-    <a href="https://www.linkedin.com/company/environmentaldashboard/" class="btn btn-primary d-flex social-btn">
-      <img src="https://environmentaldashboard.org/images/linkedin-logo.svg" alt="Environmental Dashboard on Linkedin">
-    </a>
+  <div class="col text-center d-flex justify-content-center align-items-center pb-5 social-btn-container">
+    <?php include 'social-media-links.php'; ?>
   </div>
 </div>
 <style>
