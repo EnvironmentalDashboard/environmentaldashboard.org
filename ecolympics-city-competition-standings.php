e5641a1aeeadf6134af21b01a896d5fc73074ca3
Hitesh Jangid <hiteshjangid19@gmail.com>
2024-03-04 16:11:41 +0530

    update iframe url for competition standings

--- ecolympics-city-competition-standings.php
+++ ecolympics-city-competition-standings.php
@@ -4,7 +4,7 @@
 <head>
     <meta name="description" content="Environmental Dashboard. A technology & approach for organizations and whole communities that combines feedback, through real-time public displays of resource use and environmental conditions, with thoughts and actions of community to engage, motivate, empower & celebrate sustainable thought and action.">
     <?php include 'includes/html-head.php'; ?>
-    
+
     <style>
         .rank-data {
             display: flex;
@@ -20,12 +20,12 @@
     <div class="rank-data row mb-5">
         <div class="col-md-6 pr-0">
             <div class="col-md-12 pr-0">
-                <iframe class="second-iframe" src="https://buildingos.com/blocks/28fbdab72c6440fba16a3a6c817e8b66/" allowtransparency="true" frameBorder=0 height="430"></iframe> <!-- width="520"-->
+                <iframe class="second-iframe" src="https://buildingos.com/blocks/46ff7badd92f40a8948a96cc350b091f/" allowtransparency="true" frameBorder=0 height="430"></iframe> <!-- width="520"-->
             </div>
         </div>
         <div class="col-md-6 pl-0">
             <div class="col-md-12 pl-0">
-                <iframe class="second-iframe" src="https://buildingos.com/blocks/d2245845d2db4bfba8f97c019e580a73/" allowtransparency="true" frameBorder=0 height="430"></iframe>
+                <iframe class="second-iframe" src="https://buildingos.com/blocks/b451ce33b11142ceb02332ec2fc78030/" allowtransparency="true" frameBorder=0 height="430"></iframe>
             </div>
         </div>
     </div>
